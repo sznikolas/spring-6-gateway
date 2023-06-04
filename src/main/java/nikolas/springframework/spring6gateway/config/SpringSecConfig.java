765e1ef2e0588c97b4ab69a03df1a282c2d47c6b
sznikolas <szocs.nikolas@gmail.com>
2023-06-04 17:21:33 +0200

    gateway Resource server Configuration

---
+++ src/main/java/nikolas/springframework/spring6gateway/config/SpringSecConfig.java
@@ -0,0 +1,23 @@
+package nikolas.springframework.spring6gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecConfig {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+
+        return http.build();
+    }
+}
